9858ced951add2d32fc12784eb560493cb0aa3e3
ideat <ideatsoft@gmail.com>
2021-02-26 12:04:04 -0400

    Seccion 8 terminada

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,3 +1,4 @@
+import { NgModule } from "@angular/core";
 import { PAGES_ROUTES } from './pages.routes';
 import { SharedModule } from "../shared/shared.module";
 import { FormsModule } from "@angular/forms";
@@ -5,7 +6,6 @@ import { FormsModule } from "@angular/forms";
 //ng2-charts
 import { ChartsModule } from 'ng2-charts';
 
-import { NgModule } from "@angular/core";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { Graficas1Component } from "./graficas1/graficas1.component";
 import { PagesComponent } from './pages.component';
@@ -15,6 +15,8 @@ import { ProgressComponent } from "./progress/progress.component";
 import { IncrementadorComponent } from "../components/incrementador/incrementador.component";
 import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
     declarations: [
@@ -24,7 +26,9 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
         Graficas1Component,
         IncrementadorComponent,
         GraficoDonaComponent,
-        AccountSettingsComponent
+        AccountSettingsComponent,
+        PromesasComponent,
+        RxjsComponent
     ],
     exports:[
         DashboardComponent,
